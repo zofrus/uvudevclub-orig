21bffd6b926a5fa4c90537e2b88446173bc991db
zofrus <jasonfuller7@gmail.com>
2017-11-29 23:28:24 -0700

    attempt to fix build

--- gulpfile.js
+++ gulpfile.js
@@ -37,11 +37,4 @@ gulp.task('browserSync', function() {
       baseDir: ''
     },
   })
-})
-
-// Dev task with browserSync
-gulp.task('dev', ['browserSync'], function() {
-  // Reloads the browser whenever HTML or CSS files change
-  gulp.watch('css/*.css', browserSync.reload);
-  gulp.watch('*.html', browserSync.reload);
 });
