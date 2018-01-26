53fbfa7364ffcff0ac5f7b340298fa0f2800b3b6
Mason Malone <mason.malone@yahoo.com>
2018-01-25 18:05:18 -0700

    Add member name to members list

--- members.js
+++ members.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var memberArray = ["Mitchell Daw", "Mason Malone", "Jordan Carter", "Jason Fuller", "Alex Anderson", "Steve Anderson"];
+  var memberArray = ["Mitchell Daw", "Mason Malone", "Jordan Carter", "Jason Fuller", "Alex Anderson", "Steve Anderson", "random person"];
   for(var i = 0; i < memberArray.length; i++){
     $('.memberList').append("<h2 class='section-heading'>"+ memberArray[i] + "<br></h2>");
   }
